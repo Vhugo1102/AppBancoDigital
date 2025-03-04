044d452357ce63892e5b92763707a832f8313571
Vitor Hugo da Silva <149020824+Vhugo1102@users.noreply.github.com>
2025-03-04 15:26:30 -0300

    Criada classe ContaPoupanca

---
+++ src/br/com/bancodigital/model/ContaPoupanca.java
@@ -0,0 +1,82 @@
+package br.com.bancodigital.model;
+
+
+public class ContaPoupanca {
+    private String numeroAgencia;
+    private int numeroConta;
+    private double saldo;
+    private String tipoConta;
+    private String dataAbertura;
+    private String status;
+
+    // Construtor
+    public ContaPoupanca(int numeroConta, double saldo, String tipoConta, String dataAbertura, String status) {
+        this.numeroAgencia = "0001";
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+        this.tipoConta = tipoConta;
+        this.dataAbertura = dataAbertura;
+        this.status = status;
+    }
+
+    // Getters e setters
+    public String getNumeroAgencia() {
+        return numeroAgencia;
+    }
+
+    public void setNumeroAgencia(String numeroAgencia) {
+        this.numeroAgencia = numeroAgencia;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTipoConta() {
+        return tipoConta;
+    }
+
+    public void setTipoConta(String tipoConta) {
+        this.tipoConta = tipoConta;
+    }
+
+    public String getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(String dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "ContaPoupanca{" +
+                "numeroAgencia='" + numeroAgencia + '\'' +
+                ", numeroConta=" + numeroConta +
+                ", saldo=" + saldo +
+                ", tipoConta='" + tipoConta + '\'' +
+                ", dataAbertura='" + dataAbertura + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
